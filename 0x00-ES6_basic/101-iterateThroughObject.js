9598a623facf5cb7611a0f3e8086e90cb67fa0c9
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-07-22 16:30:16 +0000

    Add and fix some tasks

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let str = "";
-  for (const emp of reportWithIterator) {
-    str += emp + " | ";
-  }
-  return str.substring(0, str.length - 3);
+  return Array.from(reportWithIterator).join(' | ');
 }
