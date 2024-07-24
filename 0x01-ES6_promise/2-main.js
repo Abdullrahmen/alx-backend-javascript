c551b2832f424cf11a187de6319df562b51d11df
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-07-24 12:39:55 +0000

    Add and fix some tasks

---
+++ 0x01-ES6_promise/2-main.js
@@ -0,0 +1,8 @@
+import handleResponseFromAPI from "./2-then";
+
+const promise = Promise.resolve();
+console.log(handleResponseFromAPI(promise));
+
+
+const promise2 = Promise.reject();
+console.log(handleResponseFromAPI(promise2));
